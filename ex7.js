c97db6ec062a794a9da3d2729080773c13f28992
omiras <oscar.miras@gmail.com>
2024-04-17 21:33:24 +0200

    fix error ty Nidaa

--- ex7.js
+++ ex7.js
@@ -4,7 +4,7 @@ Operador: && https://www.w3schools.com/js/tryit.asp?filename=tryjs_comparison_an
 
 https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Expressions_and_Operators#operadores_l%C3%B3gicos
 
-1. Escribe un valor adecuado para la variable 'usuario' y/o 'password' para que se muestre por consola el mensaje "Credenciales correctas, bievenido usuario pepe@gmail.com".
+1. Escribe un valor adecuado para la variable 'usuario' y/o 'password' para que se muestre por consola el mensaje "Credenciales correctas, bievenido usuario pepa@gmail.com".
 
 */
 
